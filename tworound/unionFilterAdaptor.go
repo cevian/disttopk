50cea4f44e0302b1eb3e3b13968cf785d177d1e2
Matvey Arye <mat@cloudflare.com>
2014-01-14 12:08:57 -0800

    fixed gcs hashtable accounting

--- tworound/unionFilterAdaptor.go
+++ tworound/unionFilterAdaptor.go
@@ -86,58 +86,55 @@ func NewBloomHistogramGcsUnionSketchAdaptor() UnionSketchAdaptor {
 
 func (t *BloomHistogramGcsUnionSketchAdaptor) getRoundTwoList(uf UnionFilter, list disttopk.ItemList, cutoff_sent int) ([]disttopk.Item, *disttopk.AlgoStats) {
 	bhc := uf.(*disttopk.BloomHistogramCollection)
-	//fmt.Println("entering get round two list")
-	list_items := list.Len()
 
 	hvf := disttopk.NewHashValueFilter()
 	bhc.AddToHashValueFilter(hvf)
 
-	if hvf.NumHashValues() < list_items {
+	//create hash table
+	ht_bits := uint8(math.Ceil(math.Log2(float64(list.Len()))))
+	ht := disttopk.NewHashTable(ht_bits)
+	for _, v := range list {
+		ht.Insert(v.Id, v.Score)
+	}
 
-		ht_bits := uint8(math.Ceil(math.Log2(float64(list_items))))
-		//ht_bits = 26 //CHANGE ME
-		ht := disttopk.NewHashTable(ht_bits)
+	hvs_sent := disttopk.NewHashValueSlice() //store hashes tested and sent here
 
-		for _, v := range list {
-			ht.Insert(v.Id, v.Score)
-		}
-		hvs_sent := disttopk.NewHashValueSlice() //hack wont store hash values
-		for i := 0; i < cutoff_sent; i++ {
-			hvs_sent.Insert(uint32(list[i].Id))
-		}
+	ids_sent := make(map[uint]bool)
+	for i := 0; i < cutoff_sent; i++ {
+		ids_sent[uint(list[i].Id)] = true
+	}
 
-		//fmt.Println("entering for loops get round two list")
+	exactlist := make([]disttopk.Item, 0)
+	items_tested := 0
+	random_access := 0
 
-		exactlist := make([]disttopk.Item, 0)
-		items_tested := 0
-		random_access := 0
-		for mod_bits, hvslice := range hvf.GetFilters() {
-			//println("Mod 2", mod_bits, hvslice.Len())
-			for _, hv := range hvslice.GetSlice() {
-				items_map, ra := ht.GetByHashValue(uint(hv), mod_bits)
-				random_access += ra
-				items_tested += len(items_map)
-				for id, score := range items_map {
-					if !hvs_sent.Contains(uint32(id)) && bhc.PassesInt(id) == true {
-						exactlist = append(exactlist, disttopk.Item{id, score})
-						hvs_sent.Insert(uint32(id))
+	for mod_bits, hf_hvslice := range hvf.GetFilters() {
+		//println("Mod 2", mod_bits, hvslice.Len())
+		for _, hf_hv := range hf_hvslice.GetSlice() {
+			table_hvs := ht.GetTableHashValues(uint(hf_hv), mod_bits)
+			for _, table_hv := range table_hvs {
+				if !hvs_sent.Contains(uint32(table_hv)) { //if we haven't processed this hv before
+					hvs_sent.Insert(uint32(table_hv))
+					random_access += 1
+
+					visitor := func(id uint, score uint) {
+						id_check := ids_sent[id]
+						if id_check == false { //not sent in previous round
+							items_tested += 1
+							if bhc.PassesInt(int(id)) {
+
+								exactlist = append(exactlist, disttopk.Item{int(id), float64(score)})
+							}
+						}
 					}
+
+					ht.VisitHashValue(table_hv, visitor)
 				}
 			}
 		}
-
-		//fmt.Println("Round two list items tested", items_tested, "random access", random_access, "total items", len(list))
-		return exactlist, &disttopk.AlgoStats{Serial_items: 0, Random_access: random_access, Random_items: items_tested /*, Length: len(list)*/}
-	} else {
-		exactlist := make([]disttopk.Item, 0)
-		for index, v := range list {
-			if index >= cutoff_sent && bhc.PassesInt(v.Id) == true {
-				exactlist = append(exactlist, disttopk.Item{v.Id, v.Score})
-			}
-		}
-		//fmt.Println("Round two list items used serial test, total items (all sequential tested)", len(list))
-		return exactlist, &disttopk.AlgoStats{Serial_items: len(list), Random_access: 0, Random_items: 0 /*, Length: len(list)*/}
 	}
+	//fmt.Println("Round two list items tested", items_tested, "random access", random_access, "total items", len(list))
+	return exactlist, &disttopk.AlgoStats{Serial_items: 0, Random_access: random_access, Random_items: items_tested}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
