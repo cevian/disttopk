447c3e97c0115af25636773d2ddebbabb9da1263
Matvey Arye <mat@cloudflare.com>
2014-01-23 22:49:49 -0800

    more printing

--- cmd/paramtest/bloomgcsmerge_test.go
+++ cmd/paramtest/bloomgcsmerge_test.go
@@ -16,7 +16,7 @@ func TestBloomGcsMergeParameter(t *testing.T) {
 	size_sum := 0
 	count := 0
 	res := ""
-	for _, listSize := range []int{ /*500000,*/ 100000, 10000, 1000} {
+	for _, listSize := range []int{500000, 100000, 10000, 1000} {
 		for _, zipfParam := range []float64{2, 1, 0.7, 0.5, 0.3} {
 			size_gcs, size_tputHash := RunBloomGcsMergeParamTest(listSize, 10, 10, zipfParam, 100)
 			improvement := (float64(size_tputHash) - float64(size_gcs)) / float64(size_tputHash)
@@ -44,70 +44,233 @@ func RunBloomGcsMergeParamTest(N, Nnodes, k int, zipParam float64, permParam int
 }
 
 type Protocol struct {
-	Name   string
-	Runner func([]disttopk.ItemList, int) (disttopk.ItemList, disttopk.AlgoStats)
+	Name    string
+	Runner  func([]disttopk.ItemList, int) (disttopk.ItemList, disttopk.AlgoStats)
+	isExact bool
 }
 
 func RunAll(N, Nnodes, k int, zipParam float64, permParam int, protos []Protocol) map[string]disttopk.AlgoStats {
 	l := disttopk.GetFullOverlapOrderPermutedSimpleList(Nnodes, uint32(N), zipParam, permParam)
 
+	naive_exact, _ := cmd.RunNaive(l, 0)
+	ground_truth := naive_exact
+
 	runtime.GC()
 	//n := Getn(l[0], k, Nnodes)
 	results := make(map[string]disttopk.AlgoStats)
 	for _, proto := range protos {
-		_, res := proto.Runner(l, k)
+		proto_list, res := proto.Runner(l, k)
+		res.CalculatePerformance(ground_truth, proto_list, k)
+		if proto.isExact && res.Abs_err != 0.0 {
+			panic(fmt.Sprintf("Protocol %v should be exact but has error %v", proto.Name, res.Abs_err))
+		}
 		results[proto.Name] = res
 	}
 
 	return results
 }
 
+var NaiveEx = Protocol{"Naive-exact", cmd.RunNaiveExact, true}
+
+//approx
+var Naive2k = Protocol{"Naive (2k)", cmd.RunNaiveK2, false}
+var Klee3 = Protocol{"Klee3", cmd.RunKlee3, false}
+var Klee4 = Protocol{"Klee4", cmd.RunKlee4, false}
+var Bloom = Protocol{"bloom", cmd.RunApproximateBloomFilter, false}
+
+// Extra-Round Exact
+var ErGcs = Protocol{"ER GCS", cmd.RunApproximateBloomGcsMergeFilter, true}
+var ErTput = Protocol{"ER TP", cmd.RunTputHashExtraRound, true}
+
+// Exact
+var Tput = Protocol{"Tput", cmd.RunTput, true}
+var TputHash = Protocol{"TputH", cmd.RunTputHash, true}
+
+//var Gcs	= Protocol{"2R Gcs  ", cmd.RunBloomSketchGcs, true}
+var GcsMerge = Protocol{"2R GcsM", cmd.RunBloomSketchGcsMerge, true}
+var CountMin = Protocol{"Count Min", cmd.RunCountMin, true}
+
 var protocols []Protocol = []Protocol{
-	//	Protocol{"Naive-exact",  runNaiveExact},
-	// Approximate:
-	//	Protocol{"Naive (2k)",   runNaiveK2},
-	Protocol{"Klee3", cmd.RunKlee3},
-	Protocol{"Klee4", cmd.RunKlee4},
-	Protocol{"bloom", cmd.RunApproximateBloomFilter},
-	// Extra-Round Exact
-	Protocol{"ER GCS", cmd.RunApproximateBloomGcsMergeFilter},
-	Protocol{"ER TP", cmd.RunTputHashExtraRound},
-	// Exact
-	Protocol{"Tput", cmd.RunTput},
-	Protocol{"TputH", cmd.RunTputHash},
-	//	Protocol{"2R Gcs  ", cmd.RunBloomSketchGcs},
-	Protocol{"2R Gcs-Merge", cmd.RunBloomSketchGcsMerge},
-	//	Protocol{"Count Min", cmd.RunCountMin},
+	Klee3,
+	Klee4,
+	Bloom,
+	ErGcs,
+	ErTput,
+	Tput,
+	TputHash,
+	GcsMerge,
+	//CountMin
+}
+
+func ApproximateProtocols() []Protocol {
+	ret := make([]Protocol, 0)
+	for _, protocol := range protocols {
+		if !protocol.isExact {
+			ret = append(ret, protocol)
+		}
+	}
+	return ret
+}
+
+func ExactProtocols() []Protocol {
+	ret := make([]Protocol, 0)
+	for _, protocol := range protocols {
+		if protocol.isExact {
+			ret = append(ret, protocol)
+		}
+	}
+	return ret
 }
 
 func TestAll(t *testing.T) {
-	res_all := MakeTitles(protocols) + "\n"
+	printers := []Printer{&OverviewPrinter{protocols, ""},
+		&ApproxPrinter{&OverviewPrinter{ApproximateProtocols(), ""}},
+		&ExactPrinter{&OverviewPrinter{ExactProtocols(), ""}},
+	}
+	for _, p := range printers {
+		p.Start()
+	}
 	for _, listSize := range []int{ /*500000, 100000, */ 10000, 1000} {
 		for _, zipfParam := range []float64{2, 1, 0.7, 0.5, 0.3} {
 			results := RunAll(listSize, 10, 10, zipfParam, 100, protocols)
-			row := MakeRow(listSize, zipfParam, protocols, results)
-			fmt.Print("Res ", row, "\n")
-			res_all += row + "\n"
+			for _, p := range printers {
+				row := p.EnterRow(RowDescription{listSize, zipfParam}, results)
+				fmt.Print("Res ", row, "\n")
+			}
 		}
-		res_all += "---------------------------------------------------------------------------------------" + "\n"
+		for _, p := range printers {
+			p.EnterNewN()
+		}
+
 		fmt.Println("=====================================")
 	}
 	fmt.Println("***********************************")
-	fmt.Print(res_all)
+
+	for _, p := range printers {
+		fmt.Print(p.Summary())
+		fmt.Println("*******************************************************************************")
+	}
+
 }
 
-func MakeTitles(protos []Protocol) string {
-	s := "N\tZip"
-	for _, proto := range protos {
-		s += "\t" + proto.Name
-	}
-	return s
+type RowDescription struct {
+	N   int
+	zip float64
 }
-func MakeRow(N int, zip float64, protos []Protocol, res map[string]disttopk.AlgoStats) string {
-	s := fmt.Sprintf("%4.1E\t%2.1f", float64(N), float64(zip))
-	for _, proto := range protos {
+
+type Printer interface {
+	Start()
+	EnterRow(rd RowDescription, res map[string]disttopk.AlgoStats) string
+	EnterNewN()
+	Summary() string
+}
+
+type OverviewPrinter struct {
+	protocols []Protocol
+	s         string
+}
+
+func (t *OverviewPrinter) EnterNewN() {
+	t.s += "---------------------------------------------------------------------------\n"
+}
+
+func (t *OverviewPrinter) Start() {
+	t.s = "N\tZip"
+	for _, proto := range t.protocols {
+		t.s += "\t" + proto.Name
+	}
+	t.s += "\n"
+}
+
+func (t *OverviewPrinter) GetRowDescription(rd RowDescription) string {
+	return fmt.Sprintf("%4.1E\t%2.1f", float64(rd.N), float64(rd.zip))
+}
+
+func (t *OverviewPrinter) EnterRow(rd RowDescription, res map[string]disttopk.AlgoStats) string {
+	s := t.GetRowDescription(rd)
+	for _, proto := range t.protocols {
 		stats := res[proto.Name]
 		s += fmt.Sprintf("\t%4.1E", float64(stats.Bytes_transferred))
 	}
+	t.s += s + "\n"
+	return s
+}
+func (t *OverviewPrinter) Summary() string {
+	return t.s
+}
+
+type ApproxPrinter struct {
+	*OverviewPrinter
+}
+
+func (t *ApproxPrinter) Start() {
+	t.s = "N\tZip"
+	for _, proto := range t.protocols {
+		t.s += "\t" + proto.Name + "\tRelE.\tRecall"
+	}
+	t.s += "\tBest BW\tBest Err\n"
+}
+
+func (t *ApproxPrinter) EnterRow(rd RowDescription, res map[string]disttopk.AlgoStats) string {
+	s := t.GetRowDescription(rd)
+	for _, proto := range t.protocols {
+		stats := res[proto.Name]
+		s += fmt.Sprintf("\t%4.1E\t%4.1E\t%2.1f", float64(stats.Bytes_transferred), float64(stats.Rel_err), float64(stats.Recall))
+	}
+	s += "\t" + t.BestProtoBytes(res) + "\t" + t.BestProtoErr(res)
+	t.s += s + "\n"
+	return s
+}
+
+func (t *OverviewPrinter) BestProtoBytes(res map[string]disttopk.AlgoStats) string {
+	bestValue := uint64(0)
+	bestName := ""
+	for _, proto := range t.protocols {
+		stats := res[proto.Name]
+		if bestValue == 0.0 || stats.Bytes_transferred < bestValue {
+			bestValue = stats.Bytes_transferred
+			bestName = proto.Name
+		}
+	}
+	return bestName
+}
+
+func (t *OverviewPrinter) BestProtoErr(res map[string]disttopk.AlgoStats) string {
+	bestValue := 0.0
+	bestName := ""
+	first := true
+	for _, proto := range t.protocols {
+
+		stats := res[proto.Name]
+		if first || stats.Abs_err < bestValue {
+			bestValue = stats.Abs_err
+			bestName = proto.Name
+			first = false
+		}
+	}
+	return bestName
+}
+
+type ExactPrinter struct {
+	*OverviewPrinter
+}
+
+func (t *ExactPrinter) Start() {
+	t.s = "N\tZip"
+	for _, proto := range t.protocols {
+		t.s += "\t" + proto.Name
+	}
+
+	t.s += "\tBest\n"
+}
+
+func (t *ExactPrinter) EnterRow(rd RowDescription, res map[string]disttopk.AlgoStats) string {
+	s := t.GetRowDescription(rd)
+	for _, proto := range t.protocols {
+		stats := res[proto.Name]
+		s += fmt.Sprintf("\t%4.1E", float64(stats.Bytes_transferred))
+	}
+	s += "\t" + t.BestProtoBytes(res)
+	t.s += s + "\n"
 	return s
 }
