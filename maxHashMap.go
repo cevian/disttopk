5eb5e3aa0ea19f8ab3eb05e2ad44f0cbedca0e56
Matvey Arye <mat@cloudflare.com>
2014-01-28 09:06:18 -0800

    Some debug info

--- maxHashMap.go
+++ maxHashMap.go
@@ -86,7 +86,7 @@ func (t *MaxHashMap) GetFilter(thresh uint) *Gcs {
 	//n := len(values)
 
 	m := (1 << (uint(t.modulus_bits)))
-	//fmt.Printf("Get Filter. m %v (%v), thresh %v, mvthresh %v, #hash values %v, #hash values above thresh %v", m, t.modulus_bits, thresh, mapValueThresh, len(t.data), len(values))
+	fmt.Printf("Get Filter. m %v (%v), thresh %v, mvthresh %v, #hash values %v, #hash values above thresh %v", m, t.modulus_bits, thresh, mapValueThresh, len(t.data), len(values))
 	gcs := NewGcs(m)
 
 	for _, value := range values {
@@ -114,6 +114,8 @@ func (t *MaxHashMap) GetThreshApprox(maxNumberHashValues int, gamma float64) uin
 	overApprox := mapValuesSorted[len(mapValuesSorted)-maxNumberHashValues]
 
 	approxThresh := underApprox + int(float64(overApprox-underApprox)*gamma)
+
+	fmt.Println("UnderApprox", underApprox, "OverApprox", overApprox, "Gamma", gamma, "Result", approxThresh)
 
 	return uint(approxThresh)
 }
