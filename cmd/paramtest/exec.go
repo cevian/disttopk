1d0de8504261103b6fffaacb0c48a7d6b40f845e
Matvey Arye <arye@cs.princeton.edu>
2014-03-03 20:14:19 -0500

    Better distributions generation

--- cmd/paramtest/exec.go
+++ cmd/paramtest/exec.go
@@ -140,7 +140,7 @@ func (t *Distribution) GetRowDescription() []RowDescription {
 	}
 
 	for _, perms := range []int{0, k, int(float64(1.5) * float64(k)), 2 * k, 4 * k, 10 * k, 100 * k} {
-		for _, overlap := range []float64{1.0, 0.75, 0.50, 0.25, 0.1, 0} {
+		for _, overlap := range []float64{1.0, 0.99, 0.75, 0.50, 0.25, 0.01, 0} {
 			for _, zipfParam := range []float64{0.2, 0.4, 0.6, 0.8, 1, 2} {
 				for _, seed := range []int64{1, 2, 3, 4, 5} {
 					for _, Lsize := range Lsizes {
