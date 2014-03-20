e970e4ca3ed16c5a7764dca77c48c051e8dcdd80
Matvey Arye <arye@cs.princeton.edu>
2014-03-20 16:50:37 -0400

    fix

--- cmd/runAll.bash
+++ cmd/runAll.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-./cmd -suite UCB -modServers 10 -keyclient 0 > UCB/run.`date +%s`.result
-./cmd -suite UCB -modServers 50 -keyclient 0 > UCB/run.`date +%s`.result
-./cmd -suite UCB -modServers 100 -keyclient 0 > UCB/run.`date +%s`.result
-./cmd -suite WC -keyclient 1 > WC/run.`date +%s`.result
+./cmd -suite UCB -modServers 10 -keyclient 0 2>&1 |tee UCB/run.`date +%s`.result
+./cmd -suite UCB -modServers 50 -keyclient 0 2>&1 |tee UCB/run.`date +%s`.result
+./cmd -suite UCB -modServers 100 -keyclient 0 2>&1 UCB/run.`date +%s`.result
+./cmd -suite WC -keyclient 1 2>&1| tee WC/run.`date +%s`.result
