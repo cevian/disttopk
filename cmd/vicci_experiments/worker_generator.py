7b1a5c42df13bab94455bfb67441270d3c052e69
Matvey Arye <arye@cs.princeton.edu>
2014-04-23 12:37:41 -0400

    vicci update

--- cmd/vicci_experiments/worker_generator.py
+++ cmd/vicci_experiments/worker_generator.py
@@ -5,35 +5,39 @@ servers = []
 #  servers.append("node"+str(i)+".mpisws.vicci.org")
 #  servers.append("node"+str(i)+".gt.vicci.org")
 
-s_as_str = """node6.gt.vicci.org
-node19.gt.vicci.org
-node15.gt.vicci.org
-node8.gt.vicci.org
-node5.gt.vicci.org
-node18.gt.vicci.org
-node14.gt.vicci.org
-node9.gt.vicci.org
-node35.gt.vicci.org
-node3.gt.vicci.org
+s_as_str = """node10.gt.vicci.org
 node1.gt.vicci.org
-node10.gt.vicci.org
-node11.gt.vicci.org
-node12.gt.vicci.org
+node2.gt.vicci.org
 node14.gt.vicci.org
-node15.gt.vicci.org
-node16.gt.vicci.org
-node17.gt.vicci.org
-node18.gt.vicci.org
-node19.gt.vicci.org
+node3.gt.vicci.org
+node5.gt.vicci.org
+node6.gt.vicci.org
 node20.gt.vicci.org
-node22.gt.vicci.org
-node24.gt.vicci.org
-node28.gt.vicci.org
-node29.gt.vicci.org
-node30.gt.vicci.org
-node31.gt.vicci.org
+node8.gt.vicci.org
 node32.gt.vicci.org
-node33.gt.vicci.org
+node24.gt.vicci.org
+node10.mpisws.vicci.org
+node11.mpisws.vicci.org
+node30.mpisws.vicci.org
+node31.mpisws.vicci.org
+node14.mpisws.vicci.org
+node15.mpisws.vicci.org
+node1.mpisws.vicci.org
+node17.mpisws.vicci.org
+node33.mpisws.vicci.org
+node19.mpisws.vicci.org
+node20.mpisws.vicci.org
+node21.mpisws.vicci.org
+node22.mpisws.vicci.org
+node23.mpisws.vicci.org
+node24.mpisws.vicci.org
+node34.mpisws.vicci.org
+node26.mpisws.vicci.org
+node2.mpisws.vicci.org
+node3.mpisws.vicci.org
+node4.mpisws.vicci.org
+node5.mpisws.vicci.org
+node6.mpisws.vicci.org
 """
 #node31.washington.vicci.org
 #node35.washington.vicci.org
