c69a1b38b72caafc54f1662ed3e47770350abaf6
Matvey Arye <arye@cs.princeton.edu>
2014-10-22 15:54:02 -0400

    backcommit

--- cmd/vicci_experiments/run_playbook_vicci.sh
+++ cmd/vicci_experiments/run_playbook_vicci.sh
@@ -1,2 +1,2 @@
 #!
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ansible_hosts_vicci -u princeton_jetstream --module-path=modules/ -f 100 $@; date
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ansible_hosts_vicci2 -u princeton_jetstream --module-path=modules/ -f 100 $@; date
